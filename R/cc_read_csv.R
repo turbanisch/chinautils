125f6f5560c87ebb3ddd7e6c305c8a60417c60ba
turbanisch <fetu@gmx.de>
2022-08-25 18:39:10 +0200

    generate column type specification based on columns present in the data
    If commodities do not come as HS8, there are no quantities given, thus the previous specification with `quantity_1 = col_double()` failed.

--- R/cc_read_csv.R
+++ R/cc_read_csv.R
@@ -1,8 +1,8 @@
 #' Read and Clean Trade Data From China Customs
 #'
-#' This is a function to read CSV files downloaded from China Customs with the correct encoding and column type specification. It expects data that was queried by month across the four dimensions commodity, partner country, province, and customs regime. The function harmonizes column names between English and Chinese files, converts dates and adds an ISO3 identifier for Chinese provinces.
+#' This is a function to read CSV files downloaded from China Customs with the correct encoding and column type specification, no matter which columns are present in the data, their order and the language. The function harmonizes column names between English and Chinese files, converts dates and adds an ISO3 identifier for Chinese provinces.
 #'
-#' Note that this function does not perform any sanity checks on the data, so make sure no individual CSV file was cut off after 10,000 lines. The function expects (and ignores) a trailing comma in each line of the CSV file which is normally added by China Customs.
+#' Note that this function does not perform any sanity checks on the data, so make sure no individual CSV file was cut off after 10,000 lines. The function expects (and ignores) a trailing comma in each line of the CSV file which is normally added by China Customs. The order of columns has to be the same across files.
 #'
 #' @param file Path to one or multiple CSV files downloaded from China Customs. Files can be in English, Chinese, or a mix of both.
 #' @param drop_descriptions Keep only codes for commodity, partner country, province, and customs regime? Default is `TRUE` because descriptions are redundant and often include spelling mistakes.
@@ -20,12 +20,18 @@ cc_read_csv <- function(paths,  drop_descriptions = TRUE) {
                         col_types = cols(.default = col_character()))
 
     valid_colnames <- colnames(first_row %>% select(!starts_with("...")))
-    language <- if (any(str_detect(valid_colnames, "\\p{script=Han}"))) "zh" else "en"
+    language <- if (any(stringr::str_detect(valid_colnames, "\\p{script=Han}"))) "zh" else "en"
 
     # get list of colnames to replace (preserving their order)
-    tibble(valid_colnames) %>%
+    clean_colnames <- tibble(valid_colnames) %>%
       left_join(chinautils::cc_variable_names, by = c("valid_colnames" = language)) %>%
       pull(clean_name)
+
+    # get specification of column types
+    col_spec <- tibble(valid_colnames) %>%
+      left_join(chinautils::cc_variable_names, by = c("valid_colnames" = language)) %>%
+      pull(col_type) %>%
+      stringr::str_c(collapse = "")
 
     # read all files
     dat <- read_csv(
@@ -33,12 +39,7 @@ cc_read_csv <- function(paths,  drop_descriptions = TRUE) {
       na = "?",
       locale = locale(encoding = "GB18030"),
       col_select = 1:length(valid_colnames),
-      col_types = cols(
-        .default = col_character(),
-        quantity_1 = col_double(),
-        quantity_2 = col_double(),
-        value = col_number()
-      ),
+      col_types = col_spec,
       col_names = clean_colnames,
       skip = 1L
     )
@@ -57,11 +58,11 @@ cc_read_csv <- function(paths,  drop_descriptions = TRUE) {
       )
 
     # sort
-    dat %>% arrange(
-      yearmonth,
-      partner,
-      province,
-      regime,
-      commodity
-    )
+    dat %>% arrange(across(any_of(c(
+      "yearmonth",
+      "partner",
+      "province",
+      "regime",
+      "commodity"
+    ))))
 }

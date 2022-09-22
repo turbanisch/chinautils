69222935e7ffe71b80b9ee410bbd242c6bad5d36
turbanisch <fetu@gmx.de>
2022-09-22 19:09:44 +0200

    add info messages with cli

--- R/countryname.R
+++ R/countryname.R
@@ -29,12 +29,37 @@ countryname <- function(sourcevar, origin = "regex", destination = "iso3c") {
     as_tibble() %>%
     join_fun(chinautils::country_dict, by = c("value" = origin))
 
-  # check multiple matches
-  multiple_matches <- matches %>%
+  # check no match
+  no_match <- matches %>% filter(is.na(short_name_en)) %>% pull(value)
+
+  # identify multiple matches
+  dupes <- matches %>%
     count(value) %>%
-    filter(n > 1)
+    filter(n > 1) %>%
+    pull(value)
 
-  # message which names could not be matched
+  # remove duplicates and set NA in case of multiple matches
+  matches <- matches %>%
+    mutate(across(!value, ~if_else(value %in% dupes, NA_character_, .x))) %>%
+    distinct()
 
+  # inform user about missings and duplicates
+  if (length(no_match) == length(sourcevar)) {
+    cli::cli_inform(c("x" = "Matched {length(sourcevar) - length(no_match)} out of {length(sourcevar)} value{?s}."))
+  } else{
+    cli::cli_inform(c("v" = "Matched {length(sourcevar) - length(no_match)} out of {length(sourcevar)} value{?s}."))
+
+    if (length(no_match) > 0) cli::cli_inform(c("x" = "Could not match {no_match}."))
+
+    if (length(dupes) > 0) {
+      cli::cli_inform(c(
+        "x" = "Found multiple matches for {dupes}.",
+        "i" = "Values with multiple matches were converted to NA."))
+    } else {
+      cli::cli_inform(c("v" = "No value had multiple matches."))
+    }
+  }
+
+  # return
   matches %>% pull(all_of(destination))
 }

cce62359441c0f4b4b6b9219030a3dcb167e8eeb
turbanisch <fetu@gmx.de>
2022-09-25 23:53:54 +0200

    Add data description

--- R/data.R
+++ R/data.R
@@ -1,22 +1,27 @@
 #' Concordance table of country names and codes.
 #'
-#' A dataset containing the names and codes of China's provinces as well as regular expressions to match province names in English, German and Chinese.
+#' A dataset containing country names in Chinese and regular expressions to match them. Each set of names in Chinese includes short and full country names in simplified and traditional scripts as used in Mainland China, Malaysia, Singapore, Hong Kong, Macau and Taiwan. The dataset additionally includes English (short) country names and ISO3 codes for harmonization purposes.
 #'
-#' @format A data frame with 34 rows and 12 variables:
+#'| Code  | Description (Chinese) | Description (English)   |
+#'|:------|:----------------------|:------------------------|
+#'| cn | 大陆简体              | Mainland (simplified)   |
+#'| hk | 香港繁體              | Hong Kong (traditional) |
+#'| mo | 澳門繁體              | Macau (traditional)     |
+#'| my | 大马简体              | Malaysia (simplified)   |
+#'| sg | 新加坡简体            | Singapore (simplified)  |
+#'| tw | 臺灣正體              | Taiwan (traditional)    |
+#'
+#' @format A data frame with 208 rows and 15 variables:
 #' \describe{
-#'   \item{iso_3166_2}{province code according to ISO 3166-2:CN}
-#'   \item{gb_2260}{province code according to Guobiao GB/T 2260}
-#'   \item{short_name_}{province name, e.g., Chongqing}
-#'   \item{full_name_}{province name including the administrative type, e.g., Chongqing Shi}
-#'   \item{_en}{province name in English}
-#'   \item{_zh}{province name in Chinese}
-#'   \item{_py}{province name in Pinyin (the official romanization system for Chinese in Mainland China)}
-#'   \item{regex}{regular expressions matching province names in English, German and Chinese}
-#'   \item{type}{administrative type, e.g., municipality}
-#'   \item{mainland}{Does the province belong to Mainland China? `FALSE` for Hong Kong, Macao and Taiwan, `TRUE` otherwise}
-#'   \item{china_customs}{numeric code used by the General Administration of Customs of China}
-#' }
-#' @source \url{https://en.wikipedia.org/wiki/Provinces_of_China}
+#'   \item{short_name_en}{Short country name in English}
+#'   \item{iso3c}{ISO3 character country code}
+#'   \item{regex}{regular expressions to match short and full country names (as well as variants thereof) after conversion to simplified characters}
+#'   \item{short_name_zh_}{Short country name in Chinese. See details for region codes.}
+#'   \item{full_name_zh_}{Full (official) country name in Chinese. See details for region codes.}
+#'   }
+#' @source
+#' \url{https://github.com/turbanisch/chinese-countryname-regex}
+#' \url{https://zh.wikipedia.org/wiki/世界政區索引}
 "country_dict"
 
 #' Concordance table of province names and codes.
